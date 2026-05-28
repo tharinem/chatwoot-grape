46a184e3df2bd4154f97692e29b6190d39cf6156
tharinem <200308306+tharinem@users.noreply.github.com>
2026-05-28 16:47:59 -0300

    fix(branding): update enforce_grape_branding with new logos and grapeai.com.br
    The before_save callback in InstallationConfig was forcing the old imgbb
    logos and reengenhariadigital URLs on every save, overriding the rake task,
    initializer and YAML. This was the root cause of branding never sticking.
    Now uses local brand-assets logos and www.grapeai.com.br URLs.

--- app/models/installation_config.rb
+++ app/models/installation_config.rb
@@ -18,11 +18,11 @@ class InstallationConfig < ApplicationRecord
   GRAPE_BRANDING = {
     'INSTALLATION_NAME' => 'Grape Ai',
     'BRAND_NAME' => 'Grape Ai',
-    'LOGO' => 'https://i.ibb.co/hFLSfwg6/Group-1.png',
-    'LOGO_DARK' => 'https://i.ibb.co/Nn3Z5mMw/Group-3.png',
-    'LOGO_THUMBNAIL' => 'https://i.ibb.co/tMLg0FFJ/Group-2.png',
-    'BRAND_URL' => 'https://www.reengenhariadigital.com.br',
-    'WIDGET_BRAND_URL' => 'https://www.reengenhariadigital.com.br'
+    'LOGO' => '/brand-assets/grape-logo-light.png',
+    'LOGO_DARK' => '/brand-assets/grape-logo-dark.png',
+    'LOGO_THUMBNAIL' => '/brand-assets/grape-thumbnail.png',
+    'BRAND_URL' => 'https://www.grapeai.com.br/',
+    'WIDGET_BRAND_URL' => 'https://www.grapeai.com.br/'
   }.freeze
 
   # https://stackoverflow.com/questions/72970170/upgrading-to-rails-6-1-6-1-causes-psychdisallowedclass-tried-to-load-unspecif
